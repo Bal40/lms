c8ca890885b11e432d3ec2009029408f9f90f5c0
Bal40 <102305523+Bal40@users.noreply.github.com>
2025-08-16 17:13:06 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-==import React from 'react'
+import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
